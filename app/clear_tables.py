d23cf9cdcaae76f85ae179d6ce49f8b512fa79b2
Ahmed <ahmedfather555@gmail.com>
2026-04-16 02:39:56 +0200

    fixes to docker related stuff

--- app/clear_tables.py
+++ app/clear_tables.py
@@ -1,4 +1,5 @@
 from sqlalchemy import delete
+from app.models import ComparisonForm
 from models.employees_model import Employee
 from database import  SessionLocal
 from models.receivingForms_model import ReceivingForm
@@ -6,7 +7,7 @@ from models.receivingForms_model import ReceivingForm
 session = SessionLocal()
 
 try:
-    session.execute(delete(ReceivingForm))
+    session.execute(delete(ComparisonForm))
     session.commit()
 finally:
     session.close()
